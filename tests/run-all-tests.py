fea2f592dd15d354eaad03a791e0bf365d10da5a
LeoBessin <leo.bessin@gmail.com>
2025-10-21 16:13:01 +0200

    add heavy testing configuration and property-based test scenarios

---
+++ tests/run-all-tests.py
@@ -0,0 +1,10 @@
+from utils import create_property_based_test
+from scenario_1 import create_player
+from scenario_2 import buy_scenario
+from scenario_3 import check_Cargaison
+
+create_property_based_test(create_player, 10)
+
+create_property_based_test(buy_scenario, 10)
+
+create_property_based_test(check_Cargaison, 1)
