fdaaaa6123aa6434a8ea3739bb54f9217ac22184
LeoBessin <leo.bessin@gmail.com>
2025-10-22 10:44:46 +0200

    refactor: rename build.yml to buildProject.yml and update test functions for improved structure and clarity

--- example/tests/run-all-tests.py
+++ example/tests/run-all-tests.py
@@ -1,10 +1,46 @@
-from utils import create_property_based_test
-from scenario_1 import create_player
-from scenario_2 import buy_scenario
-from scenario_3 import check_Cargaison
+import random
+import sys
+from pathlib import Path
 
-create_property_based_test(create_player, 10)
+# Add the parent directory to sys.path to allow imports
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
-create_property_based_test(buy_scenario, 10)
+from core.client import Game
+from tests.utils import run_test, delete_json
 
-create_property_based_test(check_Cargaison, 1)
+
+def create_player():
+    fake_name = str(random.randint(0, 2**64 - 1))
+    game = Game(fake_name)
+    status = game.get(f'/player/{game.player["playerId"]}')
+    delete_json(fake_name)
+    assert fake_name == status["name"]
+
+
+def buy_scenario():
+    fake_name = str(random.randint(0, 2**64 - 1))
+    game = Game(fake_name)
+    init_status = game.get(f'/player/{game.player["playerId"]}')
+    init_money = init_status["money"]
+    game.init_game()
+    final_status = game.get(f'/player/{game.player["playerId"]}')
+    final_money = final_status["money"]
+    print(f"Initial money: {init_money}, final money: {final_money}")
+    delete_json(fake_name)
+    assert final_money != init_money
+
+def check_cargaison():
+    fake_name = str(random.randint(0, 2**64 - 1))
+    game = Game(fake_name)
+    game.init_game()
+    print(game.get(f"/ship/{game.sid}"))
+    game.go_mine()
+    ress = game.get(f"/ship/{game.sid}")["cargo"]["resources"]
+    assert ress != {}
+    delete_json(fake_name)
+
+run_test(create_player)
+
+run_test(buy_scenario)
+
+run_test(check_cargaison)
--- example/tests/utils.py
+++ example/tests/utils.py
@@ -3,32 +3,19 @@ import sys
 import time
 from pathlib import Path
 
-NB_ITERATIONS_TESTS = 100000
-
 def delete_json(id):
     path = Path(f"{id}.json")
     path.unlink()
 
-
-def create_property_based_test(f, NB_IT=0):
-    regressions = [ ]
-    if NB_IT:
-        NB_ITERATIONS = NB_IT
-    else:
-        NB_ITERATIONS = NB_ITERATIONS_TESTS
+def run_test(f):
     start = time.time()
-    for i in range(0, NB_ITERATIONS):
-        if i < len(regressions):
-            seed = regressions[i]
-        else:
-            seed = random.randrange(0, 2**64)
-        random.seed(seed)
-        try:
-            f()
-        except AssertionError as err:
-            print(seed, "test failed")
-            print(err)
-            sys.exit(1)
-    end = time.time()
-    delta = end - start
-    print(f"{NB_ITERATIONS} iterations completed in {delta} seconds")
+    try:
+        f()
+    except AssertionError as err:
+        print(seed, "test failed")
+        print(err)
+        sys.exit(1)
+    finally:
+        end = time.time()
+        delta = end - start
+        print(f"Test runs in {delta} seconds")
